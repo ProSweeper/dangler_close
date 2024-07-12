4a507cbdf84f90ad04ae22ef92c39f3f65adfa26
ProSweeper <dylan.doucet28@gmail.com>
2024-07-12 09:06:13 -0600

    fix naming error

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { DcToggle } from "./DcToggle";
 export { DcButton } from "./DcButton";
-export { DcDropdownButton } from "./DcDropDownButton";
+export { DcDropdownButton } from "./DcDropdownButton";
