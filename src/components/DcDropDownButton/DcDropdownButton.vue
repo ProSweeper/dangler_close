c78c3d69bca4286695f0fd11f16e6b3abefc92a1
ProSweeper <dylan.doucet28@gmail.com>
2024-07-11 22:34:59 -0600

    get basic dropdown working

--- src/components/DcDropDownButton/DcDropdownButton.vue
+++ src/components/DcDropDownButton/DcDropdownButton.vue
@@ -1,12 +1,43 @@
 <template>
-	<DcButton icon-placement="right" :variant="'primary'" :label="'dropdown'">
-		<template #icon>
-			<ChevronIcon :height="15" :width="15" :fill="'white'" />
-		</template>
-	</DcButton>
+	<div class="dropdown-button">
+		<DcButton
+			icon-placement="right"
+			:color-scheme="themes.light.primary"
+			:label="'dropdown'"
+			@clicked="toggleDropdown"
+		>
+			<template #icon>
+				<ChevronIcon
+					:height="15"
+					:width="15"
+					:fill="'white'"
+					:class="[isDropdownShowing ? 'dropdownShowing' : 'dropdownHidden']"
+				/>
+			</template>
+		</DcButton>
+		<div class="dropdown-content" v-show="isDropdownShowing">
+			<slot name="dropdown-content"></slot>
+		</div>
+	</div>
 </template>
 <script setup lang="ts">
 import { DcButton } from "../../components";
 import { ChevronIcon } from "../../assets/icons";
+import { ref } from "vue";
+import { themes } from "../../constants/colors";
+
+const isDropdownShowing = ref(false);
+const toggleDropdown = () => {
+	isDropdownShowing.value = !isDropdownShowing.value;
+};
 </script>
-<style lang="" scoped></style>
+<style lang="scss" scoped>
+.dropdownShowing {
+	transform: rotate(180deg);
+	transition: transform 0.5s ease;
+}
+
+.dropdownHidden {
+	transition: transform 0.5s ease;
+}
+</style>
